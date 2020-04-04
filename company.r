fa067e1aa71cefa5cac0d5121334c8ba08c102f4
priyanshu yadav <55690378+priyanshu-data@users.noreply.github.com>
2020-04-04 23:50:18 +0530

    Update company.r

--- company.r
+++ company.r
@@ -1,3 +1,4 @@
+
 library(neuralnet)#used for neural model
 library(plyr)#used for revalue function
 library(psych)#used for cor function
@@ -68,5 +69,60 @@ head(ActualProfit_pred)
 
 
 # Improve the model performance :
+set.seed(12345)
+company_model2 <- neuralnet(Profit~R.D.Spend+Administration
+                             +Marketing.Spend+State,data = comp_train,
+                             hidden = c(4,2))
+plot(company_model2 ,rep = "best")#MSE=0.038%
+
+
+
+#improving again
+set.seed(123456)
+company_model3 <- neuralnet(Profit~R.D.Spend+Administration
+                            +Marketing.Spend+State,data = comp_train,
+                            hidden = 2)
+plot(company_model3 ,rep = "best")#MSE=0.028%
+
+#improving again
+set.seed(1234567)
+company_model4 <- neuralnet(Profit~R.D.Spend+Administration
+                            +Marketing.Spend+State,data = comp_train,
+                            hidden = c(2,2))
+plot(company_model4 ,rep = "best")#error=0.039%
+
+
+#from above model improving i can say model3 is best suited model.
+
+
+
+
+model_results3<-compute(company_model3,comp_test[1:4])#as usual 5 variable is excluded because of target varaible
+evaluate_profit3<-model_results3$net.result
+##predicted profit over actual profit
+cor(evaluate_profit3<-model_results3$net.result)
+#unnormalizing the data
+value_max3 <- max(comp_startups$Profit)
+value_min3 <- min(comp_startups$Profit)
+
+unnormalize3 <- function(x, min, max) { 
+  return( (max - min)*x + min )}
+  
+
+ActualProfit_pred3 <- unnormalize(evaluate_profit3,value_min3,value_max3)
+head(ActualProfit_pred3)
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
 
 
