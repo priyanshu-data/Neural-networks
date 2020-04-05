95fde931daa9885ede7d2368b36f4109c0e735d4
priyanshu yadav <55690378+priyanshu-data@users.noreply.github.com>
2020-04-05 22:43:37 +0530

    Update company.r

--- company.r
+++ company.r
@@ -1,120 +1,83 @@
 
-library(neuralnet)#used for neural model
-library(plyr)#used for revalue function
-library(psych)#used for cor function
+library(neuralnet)##for regression
+library(nnet)#classification
+library(NeuralNetTools)#
+library(plyr)#correlation
 
 
+#Read the data
+startups<-read.csv(choose.files())
+View(startups)
+class(startups)
 
-#loading the file in Rstudio
-comp_startups<-read.csv(choose.files())
-str(comp_startups)
-View(comp_startups)
+startups$State<-as.numeric(revalue(startups$State,
+                                   c("New York"="0","California"="1","Florida"="2")))
+startups<-as.data.frame(startups)
+attach(startups)
 
-comp_startups$State<-as.numeric(revalue(comp_startups$State),c("California"="0","Florida"="1","New York"="2"))#changing alphabet to numeric form
-
-#some visualization technique
-hist(comp_startups$R.D.Spend,probability = T,breaks = 20)
-lines(density(comp_startups$R.D.Spend))#data density is left skewed
-#same for the other variables
-
-
-#correlation of the dataset,i.e;values between -1 and 1
-cor(comp_startups)
-
-
-# Apply Normalization technique to the whole dataset :
-
+##exploratory data analysis
+plot(R.D.Spend,Profit)
+plot(Administration,Profit)
+plot(Marketing.Spend,Profit)
+plot(State,Profit)
+windows()
+pairs(startups)#finding correlation between o/p i.e;profit and inputs(3 variables)
+#corelation coefficient
+cor(startups)
+summary(startups)
+#normalizing the data set
 normalize<-function(x){
-  return ( (x-min(x))/(max(x)-min(x)))
+  return((x-min(x))/(max(x)-min(x)))
 }
-comp_startups_norm<-as.data.frame(lapply(comp_startups,FUN=normalize))
-summary(comp_startups_norm$Profit) # Normalized form of profit
+startups_norm<-as.data.frame(lapply(startups,FUN = normalize))
+summary(startups_norm$Profit)#normalized form of profit
+summary(startups$Profit)#original profit value
 
 
-##data partition
+#Data partition
 set.seed(123)
-ind <- sample(2, nrow(comp_startups_norm), replace = TRUE, prob = c(0.7,0.3))
-comp_train <- comp_startups_norm[ind==1,]
-comp_test  <- comp_startups_norm[ind==2,]
+ind<-sample(2,nrow(startups_norm),replace = TRUE,prob = c(0.7,0.3))
+startups_train<-startups_norm[ind==1,]
+startups_test<-startups_norm[ind==2,]
+#creating neural network on training data
+startups_model<-neuralnet(Profit~R.D.Spend+Administration+Marketing.Spend+State,data = startups_train)
+str(startups_model)
+plot(startups_model)
+summary(startups_model)
 
+#some fun with parameters
+par(mar = numeric(4), family = 'serif')
+plotnet(startups_model, alpha = 0.6)
 
-##building model on training data i.e;comp_train
-neural_model<-neuralnet(Profit~R.D.Spend+Administration
-                        +Marketing.Spend+State,data = comp_train)
-str(neural_model)
-plot(neural_model,rep = "best")
-
-
-# Evaluating model performance
+#evaluating model performance
 set.seed(1234)
-model_evaluate <- compute(neural_model,comp_test[1:4])#evaluation on test data i.e;comp_test###also we don't take 5 variable asit is a target vriable.
-evaluate_profit2 <- model_evaluate$net.result
+model_results<-compute(startups_model,startups_test[1:4])
+predicted_profit<-model_results$net.result
 
+#predicted profit vs actual profit of test data
+cor(predicted_profit,startups_test$Profit)##95% accuracy
 
-# Predicted profit Vs Actual profit of test data.
-cor(evaluate_profit2,comp_test$Profit)#95% accuracy obtained
+#since prediction is in normalize form ,we need to denormalize it
+#to get actual prediction on profit
+str_max<-max(startups$Profit)
+str_min<-min(startups$Profit)
 
-# since the prediction is in Normalized form, we need to de-normalize it 
-# to get the actual prediction on profit
-value_max <- max(comp_startups$Profit)
-value_min <- min(comp_startups$Profit)
+unnormalize<-function(x,min,max){return((max-min)*x+min)}
+actual_profit_pred<-unnormalize(predicted_profit,str_min,str_max)
+head(actual_profit_pred)
 
-unnormalize <- function(x, min, max) { 
-  return( (max - min)*x + min )
-}
-
-ActualProfit_pred <- unnormalize(evaluate_profit2,value_min,value_max)
-head(ActualProfit_pred)
-
-
-
-# Improve the model performance :
+#improve the model performance
 set.seed(12345)
-company_model2 <- neuralnet(Profit~R.D.Spend+Administration
-                             +Marketing.Spend+State,data = comp_train,
-                             hidden = c(4,2))
-plot(company_model2 ,rep = "best")#MSE=0.038%
+startups_model2<-neuralnet(Profit~R.D.Spend+Administration+Marketing.Spend+State,data = startups_train,hidden=2)
+plot(startups_model2,rep = "best")
+summary(startups_model2)
+model_results2<-compute(startups_model2,startups_test[1:4])
+predicted_profit2<-model_results2$net.result
+cor(predicted_profit2,startups_test$Profit)##96%accuracy
+plot(predicted_profit2,startups_test$Profit)
+par(mar =numeric(4),family='serif')
+plotnet(startups_model2,alpha = 0.6)
 
-
-
-#improving again
-set.seed(123456)
-company_model3 <- neuralnet(Profit~R.D.Spend+Administration
-                            +Marketing.Spend+State,data = comp_train,
-                            hidden = 2)
-plot(company_model3 ,rep = "best")#MSE=0.028%
-
-#improving again
-set.seed(1234567)
-company_model4 <- neuralnet(Profit~R.D.Spend+Administration
-                            +Marketing.Spend+State,data = comp_train,
-                            hidden = c(2,2))
-plot(company_model4 ,rep = "best")#error=0.039%
-
-
-#from above model improving i can say model3 is best suited model.
-
-
-
-
-model_results3<-compute(company_model3,comp_test[1:4])#as usual 5 variable is excluded because of target varaible
-evaluate_profit3<-model_results3$net.result
-##predicted profit over actual profit
-cor(evaluate_profit3<-model_results3$net.result)
-#unnormalizing the data
-value_max3 <- max(comp_startups$Profit)
-value_min3 <- min(comp_startups$Profit)
-
-unnormalize3 <- function(x, min, max) { 
-  return( (max - min)*x + min )}
-  
-
-ActualProfit_pred3 <- unnormalize(evaluate_profit3,value_min3,value_max3)
-head(ActualProfit_pred3)
-  
-  
-  
-  
   
   
   
